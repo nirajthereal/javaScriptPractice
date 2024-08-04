1439449a68778d50b14bc7e9a66dbeab47b70ff6
Niraj Kumar <nirajkumar150203@gmail.com>
2024-08-04 09:32:16 +0530

    second one

--- car.java
+++ car.java
@@ -1,6 +1,12 @@
 public class car {
 
+    public static void main(String[] args) {
+        
+    }
+
     int q = 87;
     int r = 45;
+    System.out.println("there is nthing worhty in htis ");
+
     
 }
